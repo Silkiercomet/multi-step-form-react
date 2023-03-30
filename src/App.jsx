8cbf8e390c048d76a67d282b506bf7b71bbf5585
luis Colina <colina.1990lz@gmail.com>
2023-03-30 17:25:34 -0400

    forms and functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Form1 from './components/Form1'
+import Form2 from './components/Form2'
+import Form3 from './components/Form3'
+import Form4 from './components/Form4'
+
 
 function App() {
-  const [fullForm, setFullform] = useState({name:"",email:"",phone:""})
+  const [fullForm, setFullform] = useState({name:"",email:"",phone:"",billing:{type:"",price:0, plan:""}, addon:[], total:0})
 
   useEffect(() => {
     console.log(fullForm)
@@ -11,6 +15,9 @@ function App() {
   return (
     <div className="App">
       <Form1 setFullform={setFullform}/>
+      <Form2 setFullform={setFullform}/>
+      <Form3 setFullform={setFullform}/>
+      <Form4 fullForm={fullForm} />
     </div>
   )
 }
--- src/components/Form2.jsx
+++ src/components/Form2.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import {useState} from 'react'
 import { useForm } from "react-hook-form";
-const Form2 = () => {
+
+
+
+const Form2 = ({setFullform}) => {
+  const [yearly, setYearly] = useState(false)
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors }
   } = useForm();
+
+  const OnYearly = () => setYearly(!yearly)
+  const onSubmit = (e) => {
+    const prices = {
+      "arcade": 9,
+      "advanced": 12,
+      "pro": 15
+    }
+    const value = yearly ? prices[e.plan] * 10 : prices[e.plan]
+    const type = yearly ? "yearly" : "monthly"
+    // resets addon, if is a step back is a problem  
+    const billing = {
+    type: type,
+    price:value,
+    plan:e.plan,
+    addon:[]
+    }
+    const total = billing["price"]
+    setFullform((prev) => ({...prev, billing:billing, total:total}))
+
+  }
   return (
-    <div>Form</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="arcade">arcade 65684485</label>
+        <input className='radio__input' type="radio" name="arcade" id="arcade" {...register("plan", { required: true })} value={"arcade"}/>
+        <label htmlFor="advanced"> advanced6+256468484</label>
+        <input className='radio__input' type="radio" name="advanced" id="advanced" {...register("plan", { required: true })} value={"advanced"}/>
+        <label htmlFor="pro">pro asd564a64dsa4d684</label>
+        <input className='radio__input' type="radio" name="pro" id="pro" {...register("plan", { required: true })} value={"pro"} />
+        <div>
+        <button onClick={OnYearly}>yearly is: {yearly ? "activated" : "deactivated"}</button>
+        </div>
+        <button type="submit">next step</button>
+      </form>
+
+    </div>
   )
 }
 
