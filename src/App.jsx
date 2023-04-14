12bf924fc3a2a55ff922b459d64027ac5e4062ad
luis Colina <colina.1990lz@gmail.com>
2023-04-14 17:07:42 -0400

    basic styling added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Form3 from './components/Form3'
 import Form4 from './components/Form4'
 import NumberRow from './components/NumberRow'
 import './App.css'
+import FormHeader from './components/FormHeader'
 
 function App() {
   const [fullForm, setFullform] = useState({name:"",email:"",phone:"",billing:{type:"",price:0, plan:""}, addon:[], total:0})
@@ -22,14 +23,15 @@ function App() {
   return (
     <div className="App">
       <NumberRow number={number} setNumber={setNumber} />
-      <div className="container">
+      <main className="container">
         <div className="conteiner__form" ref={formContainerRef}>
+        <FormHeader number={number} />
         {number === 1 && <Form1 setFullform={setFullform}/>}
         {number === 2 && <Form2 setFullform={setFullform}/>}
         {number === 3 && <Form3 setFullform={setFullform}/>}
         {number === 4 && <Form4 fullForm={fullForm}/>}
         </div>
-      </div>
+      </main>
 
     </div>
   )
